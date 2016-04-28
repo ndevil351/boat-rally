5e31bfb21b05b2190ad1b593e822689e41f6d462
ndevil <ndevil351@gmail.com>
2016-04-28 01:19:25 +0000

    .

--- client/maps.js
+++ client/maps.js
@@ -290,7 +290,7 @@ function updatePlacemark(msg_name, msg_text, msg_isFox, msg_FoxTimer) {
 		}, {
 			balloonPanelMaxMapArea: 0,
 			preset: "islands#blueStretchyIcon",
-			draggable: false,
+			draggable: true,
 			//draggable: (JSON.parse(msg_text).session == socket.socket.sessionid), //заглушка для теста, потом убрать, иначе все пользователи смогут таскать свои меркеры
 			//draggable: (!JSON.parse(msg_text).player_name && JSON.parse(msg_text).session == socket.socket.sessionid), //только если юзер не определен и только свою сессию
 			openEmptyBalloon: true
